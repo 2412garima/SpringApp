ba0daaf2d8948a110407b217369bd9b04e3a24b6
Garima <2412garima@gmail.com>
2025-09-06 22:05:29 +0530

    Autowiring Laptop class (instance variable in Dev) with Developer class

--- src/main/java/org/example/Developer.java
+++ src/main/java/org/example/Developer.java
@@ -3,6 +3,7 @@ package org.example;
 public class Developer {
 
     private int age;
+    private Laptop laptop;
 
     public Developer(int age) {
         this.age = age;
@@ -19,8 +20,17 @@ public class Developer {
         this.age = age;
     }
 
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
     public void build()
     {
+        laptop.compile();
         System.out.println("Building a java project");
     }
 }
--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Laptop {
+
+    public void compile()
+    {
+
+        System.out.println("Few errors found using laptop");
+    }
 }
+
+

dab5417a85d3589bbc8ac40753c70e1320f3ee3a
Garima <2412garima@gmail.com>
2025-09-06 21:54:22 +0530

    initializing bean class's attributes using constructor injection

--- src/main/java/org/example/Developer.java
+++ src/main/java/org/example/Developer.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Developer {
 
+    private int age;
+
+    public Developer(int age) {
+        this.age = age;
+    }
+
+    public Developer() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void build()
     {
         System.out.println("Building a java project");
